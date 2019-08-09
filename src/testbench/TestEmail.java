cbe9f851b9c08e101e4839862e1d1cfb382a75cd
TheOnlySmartBoy <o2jose43@gmail.com>
2019-08-09 10:12:38 +0300

    Added send email with gmail

--- src/testbench/TestEmail.java
+++ src/testbench/TestEmail.java
@@ -1,4 +1,5 @@
 package testbench;
+
 import com.sun.mail.smtp.SMTPTransport;
 
 import javax.mail.Message;
@@ -14,6 +15,7 @@ import java.util.Properties;
  * @author TheOnlySmartBoy
  */
 public class TestEmail {
+
     private static final String SMTP_SERVER = "smtp.gmail.com"; // smtp server adress/ip
     private static final String USERNAME = "o2jsmartboy@gmail.com"; //Your gmail adress
     private static final String PASSWORD = "MasterTosby2#"; //Your gmail password
@@ -26,7 +28,15 @@ public class TestEmail {
     private static final String EMAIL_TEXT = "Hello Java Mail \n ABC123";
 
     public static void main(String[] args) {
+        if (sendMail(EMAIL_FROM, EMAIL_TO, EMAIL_TO_CC, EMAIL_SUBJECT, EMAIL_TEXT)) {
+            System.out.println("Mail sent successfully");
+        } else {
+            System.out.println("Unable to send mail");
+        }
 
+    }
+
+    public static boolean sendMail(String from, String to, String cc, String subject, String message) {
         Properties prop = System.getProperties();
         prop.put("mail.smtp.starttls.enable", "true"); // Gmail nees tls to be called first
         prop.put("mail.smtp.host", SMTP_SERVER); //optional, defined in SMTPTransport
@@ -37,24 +47,24 @@ public class TestEmail {
         Message msg = new MimeMessage(session);
 
         try {
-		
-			// from
-            msg.setFrom(new InternetAddress(EMAIL_FROM));
 
-			// to 
+            // from
+            msg.setFrom(new InternetAddress(from));
+
+            // to 
             msg.setRecipients(Message.RecipientType.TO,
-                    InternetAddress.parse(EMAIL_TO, false));
+                    InternetAddress.parse(to, false));
 
-			// cc
+            // cc
             msg.setRecipients(Message.RecipientType.CC,
-                    InternetAddress.parse(EMAIL_TO_CC, false));
+                    InternetAddress.parse(cc, false));
 
-			// subject
-            msg.setSubject(EMAIL_SUBJECT);
-			
-			// content 
-            msg.setText(EMAIL_TEXT);
-			
+            // subject
+            msg.setSubject(subject);
+
+            // content 
+            msg.setText(message);
+
             msg.setSentDate(new Date());
 
             // connect
@@ -62,17 +72,18 @@ public class TestEmail {
                     SMTPTransport t = (SMTPTransport) session.getTransport("smtp")) {
                 // connect
                 t.connect(SMTP_SERVER, USERNAME, PASSWORD);
-                
+
                 // send
                 t.sendMessage(msg, msg.getAllRecipients());
-                
+
                 System.out.println("Response: " + t.getLastServerResponse());
             }
 
         } catch (MessagingException e) {
-            
+           e.printStackTrace();
+            return false;
+
         }
-
-
+        return true;
     }
 }
