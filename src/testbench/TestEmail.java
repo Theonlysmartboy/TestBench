674201ed4b44b07993fb5438a1af40db7897daaf
TheOnlySmartBoy <o2jose43@gmail.com>
2019-08-09 10:20:42 +0300

    Added send email plugin code

--- src/testbench/TestEmail.java
+++ src/testbench/TestEmail.java
@@ -17,13 +17,11 @@ import java.util.Properties;
 public class TestEmail {
 
     private static final String SMTP_SERVER = "smtp.gmail.com"; // smtp server adress/ip
-    private static final String USERNAME = "o2jsmartboy@gmail.com"; //Your gmail adress
-    private static final String PASSWORD = "MasterTosby2#"; //Your gmail password
-
+    private static final String USERNAME = ""; //Your gmail adress
+    private static final String PASSWORD = ""; //Your gmail password
     private static final String EMAIL_FROM = "info@otemainc.com";
     private static final String EMAIL_TO = "tosby@otemainc.com";
     private static final String EMAIL_TO_CC = "";
-
     private static final String EMAIL_SUBJECT = "Test Send Email via SMTP";
     private static final String EMAIL_TEXT = "Hello Java Mail \n ABC123";
 
@@ -33,56 +31,41 @@ public class TestEmail {
         } else {
             System.out.println("Unable to send mail");
         }
-
     }
-
     public static boolean sendMail(String from, String to, String cc, String subject, String message) {
         Properties prop = System.getProperties();
         prop.put("mail.smtp.starttls.enable", "true"); // Gmail nees tls to be called first
         prop.put("mail.smtp.host", SMTP_SERVER); //optional, defined in SMTPTransport
         prop.put("mail.smtp.auth", "true");
         prop.put("mail.smtp.port", "587"); // default port is 25 but to use TLS we need port 587
-
         Session session = Session.getInstance(prop, null);
         Message msg = new MimeMessage(session);
-
         try {
-
             // from
             msg.setFrom(new InternetAddress(from));
-
             // to 
             msg.setRecipients(Message.RecipientType.TO,
                     InternetAddress.parse(to, false));
-
             // cc
             msg.setRecipients(Message.RecipientType.CC,
                     InternetAddress.parse(cc, false));
-
             // subject
             msg.setSubject(subject);
-
             // content 
             msg.setText(message);
-
             msg.setSentDate(new Date());
-
             // connect
             try ( // Get SMTPTransport
                     SMTPTransport t = (SMTPTransport) session.getTransport("smtp")) {
                 // connect
                 t.connect(SMTP_SERVER, USERNAME, PASSWORD);
-
                 // send
                 t.sendMessage(msg, msg.getAllRecipients());
-
                 System.out.println("Response: " + t.getLastServerResponse());
             }
-
         } catch (MessagingException e) {
-           e.printStackTrace();
+            e.printStackTrace();
             return false;
-
         }
         return true;
     }
