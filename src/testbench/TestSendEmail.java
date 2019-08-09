e3988b0bd4cb5eb30ba18219bb34d529cf42c3a7
TheOnlySmartBoy <o2jose43@gmail.com>
2019-08-09 18:49:43 +0300

    Added smtp security

--- src/testbench/TestSendEmail.java
+++ src/testbench/TestSendEmail.java
@@ -1,6 +1,5 @@
 package testbench;
 
-import com.sun.mail.smtp.SMTPTransport;
 
 import javax.mail.Message;
 import javax.mail.MessagingException;
@@ -9,6 +8,8 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import java.util.Date;
 import java.util.Properties;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Transport;
 
 /**
  *
@@ -20,10 +21,10 @@ public class TestSendEmail {
     private static final String USERNAME = ""; //Your gmail adress
     private static final String PASSWORD = ""; //Your gmail password
     private static final String EMAIL_FROM = "info@otemainc.com";
-    private static final String EMAIL_TO = "tosby@otemainc.com";
-    private static final String EMAIL_TO_CC = "";
+    private static final String EMAIL_TO = "o2jose43@gmail.com";
+    private static final String EMAIL_TO_CC = "tosby@otemainc.com";
     private static final String EMAIL_SUBJECT = "Test Send Email via SMTP";
-    private static final String EMAIL_TEXT = "Hello Java Mail \n ABC123";
+    private static final String EMAIL_TEXT = "Hello Java Mail \n It works";
 
     public static void main(String[] args) {
         if (sendMail(EMAIL_FROM, EMAIL_TO, EMAIL_TO_CC, EMAIL_SUBJECT, EMAIL_TEXT)) {
@@ -32,37 +33,45 @@ public class TestSendEmail {
             System.out.println("Unable to send mail");
         }
     }
+
     public static boolean sendMail(String from, String to, String cc, String subject, String message) {
-        Properties prop = System.getProperties();
+        Properties prop = new Properties();
+
+        prop.put("mail.smtp.auth", "true");
+        prop.put("mail.debug", "true");
         prop.put("mail.smtp.starttls.enable", "true"); // Gmail nees tls to be called first
         prop.put("mail.smtp.host", SMTP_SERVER); //optional, defined in SMTPTransport
-        prop.put("mail.smtp.auth", "true");
-        prop.put("mail.smtp.port", "587"); // default port is 25 but to use TLS we need port 587
-        Session session = Session.getInstance(prop, null);
-        Message msg = new MimeMessage(session);
+        prop.put("mail.smtp.socketFactory.port", "465");
+        prop.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        prop.put("mail.smtp.port", "465"); // default port is 25 but to use TLS we need port 587
+        Session session = Session.getDefaultInstance(prop,
+                new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(USERNAME, PASSWORD);
+            }
+        });
+        MimeMessage msg = new MimeMessage(session);
         try {
             // from
             msg.setFrom(new InternetAddress(from));
             // to 
-            msg.setRecipients(Message.RecipientType.TO,
-                    InternetAddress.parse(to, false));
+            msg.addRecipient(Message.RecipientType.TO,
+                   new InternetAddress(to));
             // cc
-            msg.setRecipients(Message.RecipientType.CC,
-                    InternetAddress.parse(cc, false));
+            msg.addRecipient(Message.RecipientType.CC,
+                   new InternetAddress(cc));
             // subject
             msg.setSubject(subject);
             // content 
             msg.setText(message);
             msg.setSentDate(new Date());
             // connect
-            try ( // Get SMTPTransport
-                    SMTPTransport t = (SMTPTransport) session.getTransport("smtp")) {
-                // connect
-                t.connect(SMTP_SERVER, USERNAME, PASSWORD);
-                // send
-                t.sendMessage(msg, msg.getAllRecipients());
-                System.out.println("Response: " + t.getLastServerResponse());
-            }
+            // Get SMTPTransport
+            //SMTPTransport t = (SMTPTransport) session.getTransport("smtp");
+            // connect
+            // send
+            Transport.send(msg, msg.getAllRecipients());
+            System.out.println("Mail sent successfuly ");
         } catch (MessagingException e) {
             e.printStackTrace();
             return false;
